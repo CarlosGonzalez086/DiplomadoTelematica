f85c2a024c036933119539847c8ca39641607902
CarlosGonzalez88 <101305205+CarlosGonzalez88@users.noreply.github.com>
2022-05-22 21:15:33 -0500

    Update GraficarPuntos.py

--- 4roots/2do/GraficarPuntos.py
+++ 4roots/2do/GraficarPuntos.py
@@ -3,9 +3,10 @@
 
 import matplotlib.pyplot as plp
 import numpy as np
+from math import sin,sqrt
 
 X = np.arange(0, 10, .10)
-Y = X * 3
+Y = (X + sin(2)) * sqrt(9)
 
 print("Puntos X:", X)
 print("Puntos Y:", Y)
